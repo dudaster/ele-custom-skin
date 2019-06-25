2f66b2b5004f89d8c6df7f94af507c95bbd77f6b
Liviu Duda <dudaster@gmail.com>
2019-06-25 16:05:11 +0300

    v1.1.0
    latest version from wordpress repository

--- theme-builder/init.php
+++ theme-builder/init.php
@@ -1,11 +1,11 @@
 <?php
 
-require_once MY_WIDGETS.'theme-builder/documents/loop.php';
-require_once MY_WIDGETS.'theme-builder/dynamic-tags/ele-tags.php';
+require_once ELECS_DIR.'theme-builder/documents/loop.php';
+require_once ELECS_DIR.'theme-builder/dynamic-tags/ele-tags.php';
 //add new tags
 $newtags=new ElementorPro\Modules\DynamicTags\Eletags();
 $newtags::instance();
-//require_once MY_WIDGETS.'theme-builder/classes/custom-types-manager.php';
+//require_once ELECS_DIR.'theme-builder/classes/custom-types-manager.php';
 
 use Elementor\TemplateLibrary\Source_Local;
 use ElementorPro\Modules\ThemeBuilder\Documents\Loop;
@@ -15,7 +15,7 @@ use ElementorPro\Modules\ThemeBuilder\Documents\Theme_Document;
 Plugin::elementor()->documents->register_document_type( 'loop', Loop::get_class_full_name() );
 Source_Local::add_template_type( 'loop' );
 
-function my_get_document( $post_id ) {
+function elecs_get_document( $post_id ) {
 		$document = null;
 
 		try {
@@ -29,9 +29,9 @@ function my_get_document( $post_id ) {
 		return $document;
 	}
 
-function add_more_types($settings){
+function elecs_add_more_types($settings){
   $post_id = get_the_ID();
-  $document = my_get_document( $post_id );
+  $document = elecs_get_document( $post_id );
 
 	if ( ! $document ) {
 		return $settings;
@@ -39,8 +39,8 @@ function add_more_types($settings){
   
   $new_types=['loop'=>Loop::get_properties()];
   $add_settings=['theme_builder' => ['types' =>$new_types]];
-  $settings = array_merge_recursive($settings, $add_settings);
+  if (!array_key_exists('loop', $settings['theme_builder']['types'])) $settings = array_merge_recursive($settings, $add_settings);
   return $settings;
 }
 
-add_filter( 'elementor_pro/editor/localize_settings', 'add_more_types' );
+add_filter( 'elementor_pro/editor/localize_settings', 'elecs_add_more_types' );
